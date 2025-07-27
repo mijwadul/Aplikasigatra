50178d5952198a779a8e7d2a6a2a92e1b2f0b1c8
Mijwadul <mijwadul@gmail.com>
2025-07-28 02:09:28 +0700

    comat camit

---
+++ backend/app/models/__init__.py
@@ -0,0 +1,4 @@
+from .pdf_reference import PDFReference
+from .user import User
+from .school import School
+from .class_model import Class
---
+++ backend/app/models/school.py
@@ -0,0 +1,13 @@
+from app.extensions import db
+
+class School(db.Model):
+    __tablename__ = 'school'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(150), unique=True, nullable=False)
+    address = db.Column(db.String(255), nullable=True)
+    
+    users = db.relationship('User', back_populates='school')
+    classes = db.relationship('Class', back_populates='school', cascade="all, delete-orphan") # Add this line
+
+    def __repr__(self):
+        return f'<School {self.name}>'
